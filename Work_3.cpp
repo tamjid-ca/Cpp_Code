47636fe869ef75d0fb69d93d9fccf74faabe6cef
Tamjid <mdshahadathossain@gmail.com>
2026-02-21 09:14:00 -0700

    Hi, this is a commit message.

---
+++ Work_3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string food;
+    cout << "What is your favorite food? ";
+    cin >> food;
+    cout << "I love eating " << food << " too!";
+}
